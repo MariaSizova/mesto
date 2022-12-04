5e6986c5f8aac915328508f224b7d21c52a7a170
Maria Sizova <sizova.maria.olegovna@yandex.ru>
2022-12-04 22:39:29 +0300

    save: добавлен 2 попап и начало верстки 3 попап

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,73 @@
-// попап
-
-const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-btn');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input_type_surname');
-const jobInput = formElement.querySelector('.popup__input_type_profession');
+// DOM узел попап 1
+const popupElementProfile = document.querySelector('.popup_type_profile');
+const popupCloseButtonElementProfile = popupElementProfile.querySelector('.popup__close-btn');
+const popupOpenButtonElementProfile = document.querySelector('.profile__edit-button');
+const formElementProfile = document.querySelector('.popup__form');
+const nameInput = formElementProfile.querySelector('.popup__input_type_surname');
+const jobInput = formElementProfile.querySelector('.popup__input_type_profession');
 const profileTitle = document.querySelector('.profile__title');
 const profileProfession = document.querySelector('.profile__profession');
 
-const openPopup = function () {
-  popupElement.classList.add('popup_is-opened');
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileProfession.textContent;
+// DOM узел попап 2
+const popupElementAddPlace = document.querySelector('.popup_type_add-place');
+const popupCloseButtonElementAddPlace = popupElementAddPlace.querySelector('.popup__close-btn');
+const popupOpenButtonElementAddPlace = document.querySelector('.profile__add-button');
+const formElementAddPlace = document.querySelector('.popup__form');
+const pictureInput = document.querySelector('.popup__input_type_place-name');
+const linkInput = document.querySelector('.popup__input_type_link');
+
+// Функция открытие попап 1
+const openPopupProfile = () => {
+  popupElementProfile.classList.add('popup_is-opened'); // Добавлет класс для отображения попап
+  nameInput.value = profileTitle.textContent; // Берет значение имени из section:profile
+  jobInput.value = profileProfession.textContent; // Берет значение профессии из section:profile
 }
 
-const closePopup = function () {
-  popupElement.classList.remove('popup_is-opened');
+// Функция открытия попап 2
+const openPopupAddPlace = () => {
+  popupElementAddPlace.classList.add('popup_is-opened'); // Добавлет класс для отображения попап
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
+// Функция закрытия попап 1
+const closePopupProfile = () => {
+  popupElementProfile.classList.remove('popup_is-opened'); // Удаляет класс для отображения попап
+}
 
-//Редактирование имени и информации о себе
+// Функция закрытия попап 2
+const closePopupAddPlace = () => {
+  popupElementAddPlace.classList.remove('popup_is-opened'); // Удаляет класс для отображения попап
+}
 
+// Кнопка открытия попап 1
+popupOpenButtonElementProfile.addEventListener('click', openPopupProfile); // По клику на кнопку запускает функцию открытия попап 1
+
+// Кнопка открытия попап 2
+popupOpenButtonElementAddPlace.addEventListener('click', openPopupAddPlace); // По клику на кнопку запускает функцию открытия попап 2
+
+// Кнопка закрытия попап 1
+popupCloseButtonElementProfile.addEventListener('click', closePopupProfile); // По клику на кнопку запускает функцию закрытия попап 1
+
+// Кнопка закрытия попап 2
+popupCloseButtonElementAddPlace.addEventListener('click', closePopupAddPlace); // По клику на кнопку запускает функцию закрытия попап 2
+
+//Редактирование имени и информации о себе попап 1
 function formSubmitHandler(evt) {
-  evt.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileProfession.textContent = jobInput.value;
-  closePopup();
+  evt.preventDefault(); // Отменить действие браузера по умолчанию после наступления события
+  profileTitle.textContent = nameInput.value; // Применяем к фамилии в профиле значение введенное в форме попап 1
+  profileProfession.textContent = jobInput.value; // Применяем к профессии в профиле значение введенное в форме попап 1
+  closePopupProfile(); //Автоматическое закрытие попап 1
 }
 
-popupCloseButtonElement.addEventListener('click', closePopup);
+// Кнопка отправки формы попап 1
+formElementProfile.addEventListener('submit', formSubmitHandler);
 
-formElement.addEventListener('submit', formSubmitHandler);
+// Добавление новой карточки попап 2
+
+
+
+
+
+
 
 // Массив карточек
 
